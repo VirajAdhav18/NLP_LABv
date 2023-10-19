6e5ded62a72e742d10986179ee63d49e3be87a47
VirajAdhav18 <105536568+VirajAdhav18@users.noreply.github.com>
2023-10-19 15:31:00 +0530

    assignment3

--- assignment1.py
+++ assignment1.py
@@ -1,234 +1,115 @@
+# Name:-Adhav Viraj Vikram
+# Roll no:-01
+# Class:-B.Tech
+# Assignment No:-02
+#Title: Assignment to implemnt Bag of Words TF-ID Using Gensim library.
+from gensim.utils import simple_preprocess
+from gensim import corpora, models
+import numpy as np
 
-##Assignment No.01##
-#Name:Adhav Viraj Vikram
-##Roll No:01
-#Batch:B1
-#Title:"Text Pre-Processing using NLP operations:perform Tokenization
-# Stop word removal,Lemmatization ,Part-of-Speech Tagging use any sample text"
+# Bag-of-Words
 
-#import Libraries
-import spacy
-#language model loaded
-nlp = spacy.load("en_core_web_sm")
-about_text = (
-   "My name is viraj and I am from kopargaon and I am studying Btech in Information Technology."
-    "who is your friend and where does he lives in the town."
-)
-##1-------Tokenization---------##
+#text2 = open('sample_text.txt', encoding='utf-8')
 
-about_doc = nlp(about_text)
+text2 = ["""natural language processing (NLP) is a field of artificial intelligence that focuses on the 
+            interaction between computers and humans through natural language. It enables machines to understand,
+            interpret, and generate human language in a valuable way."""]
 
-for token in about_doc:
-    print (token, token.idx)
+tokens2 = []
+# for line in text2.read().split('.'):
+for line in text2[0].split('.'):
+    tokens2.append(simple_preprocess(line, deacc=True))
 
+g_dict2 = corpora.Dictionary(tokens2)
 
-##2-------Stop Words-----------###
+print("The dictionary has: " + str(len(g_dict2)) + " tokens")
+print(g_dict2.token2id)
+print("\n")
 
-about_doc = nlp(about_text)
-print([token for token in about_doc if not token.is_stop])
+g_bow =[g_dict2.doc2bow(token, allow_update = True) for token in tokens2]
+print("Bag of Words : ", g_bow)
 
 
-##3-------Lemmatization-----------##
 
-about_doc=nlp(about_text)
-for token in about_doc:
-    if str(token) != str(token.lemma_):
-        print(f"{str(token):>20} : {str(token.lemma_)}")
-        
+#Term Frequency – Inverse Document Frequency (TF-ID)
 
- ##4-------Part of Speech----------##   
-               
-about_doc = nlp(about_text)
-for token in about_doc:
-    print(
-        f"""
-TOKEN: {str(token)}
-=====
-TAG: {str(token.tag_):10} POS: {token.pos_}
-EXPLANATION: {spacy.explain(token.tag_)}"""
-    )
+text = [
+        "The weather today is sunny and warm, perfect for outdoor activities.",
+        "I have a lot of work to do, and the deadline is approaching fast.",
+        "I enjoy reading books in my free time, especially science fiction novels."
+        ]
 
-#OUTPUT
-#    My 0
-# name 3
-# is 8
-# viraj 11
-# and 17
-# I 21
-# am 23
-# from 26
-# kopargaon 31
-# and 41
-# I 45
-# am 47
-# studying 50
-# Btech 59
-# in 65
-# Information 68
-# Technology.who 80
-# is 95
-# your 98
-# friend 103
-# and 110
-# where 114
-# does 120
-# he 125
-# lives 128
-# in 134
-# the 137
-# town 141
-# . 145
-# [viraj, kopargaon, studying, Btech, Information, Technology.who, friend, lives, town, .]
-#                   My : my
-#                   is : be
-#                   am : be
-#                   am : be
-#             studying : study
-#                   is : be
-#                 does : do
-#                lives : live
+g_dict = corpora.Dictionary([simple_preprocess(line) for line in text])
+g_bow = [g_dict.doc2bow(simple_preprocess(line)) for line in text]
 
-# TOKEN: My
-# =====
-# TAG: PRP$       POS: PRON
-# EXPLANATION: pronoun, possessive
+print("\nTerm Frequency – Inverse Document Frequency (TF-ID)")
+print("Dictionary : ")
+for item in g_bow:
+    print([[g_dict[id], freq] for id, freq in item])
 
-# TOKEN: name
-# =====
-# TAG: NN         POS: NOUN
-# EXPLANATION: noun, singular or mass
+g_tfidf = models.TfidfModel(g_bow, smartirs='ntc')
 
-# TOKEN: is
-# =====
-# TAG: VBZ        POS: AUX
-# EXPLANATION: verb, 3rd person singular present
+print("\n TF-IDF Vector:")
+for item in g_tfidf[g_bow]:
+    print([[g_dict[id], np.around(freq, decimals=2)] for id, freq in item])
 
-# TOKEN: viraj
-# =====
-# TAG: JJ         POS: ADJ
-# EXPLANATION: adjective (English), other noun-modifier (Chinese)
 
-# TOKEN: and
-# =====
-# TAG: CC         POS: CCONJ
-# EXPLANATION: conjunction, coordinating
 
-# TOKEN: I
-# =====
-# TAG: PRP        POS: PRON
-# EXPLANATION: pronoun, personal
+#word2vec
 
-# TOKEN: am
-# =====
-# TAG: VBP        POS: AUX
-# EXPLANATION: verb, non-3rd person singular present
+from gensim.models.word2vec import Word2Vec
+from multiprocessing import cpu_count
 
-# TOKEN: from
-# =====
-# TAG: IN         POS: ADP
-# EXPLANATION: conjunction, subordinating or preposition
+print("\n Word2vec \n")
 
-# TOKEN: kopargaon
-# =====
-# TAG: NNP        POS: PROPN
-# EXPLANATION: noun, proper singular
+data = [
+    "this is a sentence",
+    "another example sentence",
+    "word embeddings are useful",
+    "word2vec is a popular model",
+]
 
-# TOKEN: and
-# =====
-# TAG: CC         POS: CCONJ
-# EXPLANATION: conjunction, coordinating
+# Tokenize the text data (split sentences into words)
+tokenized_data = [sentence.split() for sentence in data]
 
-# TOKEN: I
-# =====
-# TAG: PRP        POS: PRON
-# EXPLANATION: pronoun, personal
+# Create a Word2Vec model
+w2v_model = Word2Vec(tokenized_data, min_count=0, workers=cpu_count())
 
-# TOKEN: am
-# =====
-# TAG: VBP        POS: AUX
-# EXPLANATION: verb, non-3rd person singular present
+# Find the most similar words to 'word'
+similar_words = w2v_model.wv.most_similar('word')
 
-# TOKEN: studying
-# =====
-# TAG: VBG        POS: VERB
-# EXPLANATION: verb, gerund or present participle
+for word, score in similar_words:
+    print(f"{word}: {score}")
 
-# TOKEN: Btech
-# =====
-# TAG: NNP        POS: PROPN
-# EXPLANATION: noun, proper singular
+# OUTPUT:--
+# The dictionary has: 30 tokens
+# {'and': 0, 'artificial': 1, 'between': 2, 'computers': 3, 'field': 4, 'focuses': 5, 'humans': 6, 'intelligence': 7, 'interaction': 8, 'is': 9, 'language': 10, 'natural': 11, 'nlp': 12, 'of': 13, 'on': 14, 'processing': 15, 'that': 16, 'the': 17, 'through': 18, 'enables': 19, 'generate': 20, 'human': 21, 'in': 22, 'interpret': 23, 'it': 24, 'machines': 25, 'to': 26, 'understand': 27, 'valuable': 28, 'way': 29}
 
-# TOKEN: in
-# =====
-# TAG: IN         POS: ADP
-# EXPLANATION: conjunction, subordinating or preposition
 
-# TOKEN: Information
-# =====
-# TAG: NNP        POS: PROPN
-# EXPLANATION: noun, proper singular
+# Bag of Words :  [[(0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (9, 1), (10, 2), (11, 2), (12, 1), (13, 1), (14, 1), (15, 1), (16, 1), (17, 1), (18, 1)], [(0, 1), (10, 1), (19, 1), (20, 1), (21, 1), (22, 1), (23, 1), (24, 1), (25, 1), (26, 1), (27, 1), (28, 1), (29, 1)], []]
 
-# TOKEN: Technology.who
-# =====
-# TAG: NNP        POS: PROPN
-# EXPLANATION: noun, proper singular
+# Term Frequency � Inverse Document Frequency (TF-ID)
+# Dictionary : 
+# [['activities', 1], ['and', 1], ['for', 1], ['is', 1], ['outdoor', 1], ['perfect', 1], ['sunny', 1], ['the', 1], ['today', 1], ['warm', 1], ['weather', 1]]
+# [['and', 1], ['is', 1], ['the', 1], ['approaching', 1], ['deadline', 1], ['do', 1], ['fast', 1], ['have', 1], ['lot', 1], ['of', 1], ['to', 1], ['work', 1]]
+# [['books', 1], ['enjoy', 1], ['especially', 1], ['fiction', 1], ['free', 1], ['in', 1], ['my', 1], ['novels', 1], ['reading', 1], ['science', 1], ['time', 1]]
 
-# TOKEN: is
-# =====
-# TAG: VBZ        POS: AUX
-# EXPLANATION: verb, 3rd person singular present
+#  TF-IDF Vector:
+# [['activities', 0.34], ['and', 0.17], ['for', 0.34], ['is', 0.17], ['outdoor', 0.34], ['perfect', 0.34], ['sunny', 0.34], ['the', 0.17], ['today', 0.34], ['warm', 0.34], ['weather', 0.34]]
+# [['and', 0.16], ['is', 0.16], ['the', 0.16], ['approaching', 0.32], ['deadline', 0.32], ['do', 0.32], ['fast', 0.32], ['have', 0.32], ['lot', 0.32], ['of', 0.32], ['to', 0.32], ['work', 0.32]]
+# [['books', 0.3], ['enjoy', 0.3], ['especially', 0.3], ['fiction', 0.3], ['free', 0.3], ['in', 0.3], ['my', 0.3], ['novels', 0.3], ['reading', 0.3], ['science', 0.3], ['time', 0.3]]
 
-# TOKEN: your
-# =====
-# TAG: PRP$       POS: PRON
-# EXPLANATION: pronoun, possessive
+#  Word2vec 
 
-# TOKEN: friend
-# =====
-# TAG: NN         POS: NOUN
-# EXPLANATION: noun, singular or mass
+# sentence: 0.21617141366004944
+# embeddings: 0.044689226895570755
+# example: 0.015025208704173565
+# useful: 0.0019510718993842602
+# is: -0.03284316137433052
+# this: -0.04568909481167793
+# another: -0.0742427185177803
+# are: -0.09326908737421036
+# a: -0.09575342386960983
+# word2vec: -0.10513807833194733
 
-# TOKEN: and
-# =====
-# TAG: CC         POS: CCONJ
-# EXPLANATION: conjunction, coordinating
 
-# TOKEN: where
-# =====
-# TAG: WRB        POS: SCONJ
-# EXPLANATION: wh-adverb
-
-# TOKEN: does
-# =====
-# TAG: VBZ        POS: AUX
-# EXPLANATION: verb, 3rd person singular present
-
-# TOKEN: he
-# =====
-# TAG: PRP        POS: PRON
-# EXPLANATION: pronoun, personal
-
-# TOKEN: lives
-# =====
-# TAG: VBZ        POS: VERB
-# EXPLANATION: verb, 3rd person singular present
-
-# TOKEN: in
-# =====
-# TAG: IN         POS: ADP
-# EXPLANATION: conjunction, subordinating or preposition
-
-# TOKEN: the
-# =====
-# TAG: DT         POS: DET
-# EXPLANATION: determiner
-
-# TOKEN: town
-# =====
-# TAG: NN         POS: NOUN
-# EXPLANATION: noun, singular or mass
-
-# TOKEN: .
-# =====
-# TAG: .          POS: PUNCT
-# EXPLANATION: punctuation mark, sentence closer
